3b1de6570db491f86691f03d4b7927bb83158c68
livbogdan <livbogdan@gmail.com>
2023-10-01 22:11:47 +0200

    Initial Commit

---
+++ alarmApp/alarmAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  alarmAppApp.swift
+//  alarmApp
+//
+//  Created by Bogdan Livanov on 2023-10-01.
+//
+
+import SwiftUI
+
+@main
+struct alarmAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
