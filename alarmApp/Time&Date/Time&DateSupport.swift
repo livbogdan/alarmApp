f312abfc0e8583b17a5f796df458efbbd57e582f
livbogdan <livbogdan@gmail.com>
2023-10-03 13:23:10 +0200

    Added utility functions
    This commit adds essential date and time utility functions and a SwiftUI view for displaying time-of-day icons.
    
    - `getTimeFromDate`: A function that formats a Date into a string representation of time in "HH:mm" format.
    
    - `addHourToDate`: A function that adds a specified number of hours and minutes to a Date.
    
    - `getTimeComponents`: A function that extracts various date components (hour, minute, day, month, year) from a Date using Calendar.current.
    
    - `dateToPercent`: A function that converts a Date into a percentage of the day (ranging from 0.0 to 1.0).
    
    - `TimeOfDayIcon`: A SwiftUI view that displays icons representing different times of day based on a given Date.
    
    - Included comments and documentation for better code readability and understanding.
    
    These utilities are designed to be reusable and enhance the functionality of our project by providing convenient date and time handling.

---
+++ alarmApp/Time&Date/Time&DateSupport.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+// Function to format a given Date into a string representation of time (HH:mm).
+func getTimeFromDate(date: Date) -> String {
+    // Extract the hour and minute components from the date.
+    let result = getTimeComponents(date: date)
+    let hour = result.hour
+    let minute = result.minute
+    
+    // Format the time as "HH:mm".
+    let time = String(format: "%02d:%02d", hour, minute)
+    
+    return time
+}
+
+// Function to add a specified number of hours and minutes to a given Date.
+func addHourToDate(date: Date, numHours: Int, numMinutes: Int) -> Date {
+    // Calculate the time interval to add to the date in seconds.
+    let timeInterval = TimeInterval(numMinutes * 60 + numHours * 60 * 60)
+    
+    // Add the calculated time interval to the date.
+    return date.addingTimeInterval(timeInterval)
+}
+
+// Function to extract various components (hour, minute, day, month, year) from a Date.
+func getTimeComponents(date: Date) -> (hour: Int, minute: Int, day: Int, month: Int, year: Int) {
+    // Use Calendar.current to extract the desired date components from the input date.
+    let components = Calendar.current.dateComponents(
+        [.hour,.month,.year, .day,.minute], from: date)
+    
+    // Extract individual components (default to 0 if not found).
+    let hour = components.hour ?? 0
+    let minute = components.minute ?? 0
+    let day = components.day ?? 0
+    let month = components.month ?? 0
+    let year = components.year ?? 0
+    
+    // Return the extracted components as a tuple.
+    return (hour: hour, minute: minute, day: day, month: month, year: year)
+}
+
+// Function to convert a Date into a percentage of the day (0.0 to 1.0).
+func dateToPercent(date: Date) -> CGFloat {
+    // Extract the hour and minute components from the date.
+    let result = getTimeComponents(date: date)
+    
+    // Calculate the percentage of the day based on the hour and minute.
+    return CGFloat(result.hour) / 24 + CGFloat(result.minute) / (60*24)
+}
---
+++ alarmApp/Views/Supporting Views/TimeOfDayIcon.swift
@@ -0,0 +1,54 @@
+import SwiftUI
+
+// A SwiftUI view for displaying icons representing different times of day based on the given date.
+struct TimeOfDayIcon: View {
+    let date: Date
+    var precent: CGFloat{
+        dateToPercent(date: date)
+    }
+    var hour: Int {
+        Int(24*precent)
+    }
+    
+    // Determine the image and color based on the hour of the day.
+    var image: (name: String, color: Color){
+        switch(hour) {
+        case 6..<8:
+            return ("sunrise.fill", .orange)
+        case 8..<17:
+            return ("sun.max.fill", .yellow)
+        case 17..<20:
+            return ("sun.and.horizon.fill", .pink)
+        case 20..<23:
+            return ("moon.fill", .black)
+        default:
+            return ("moon.stars.fill", .black)
+            
+        }
+    }
+    
+    // The body of the view, which displays an image with the determined name and color.
+    var body: some View {
+        Image(systemName: image.name)
+            .foregroundColor(image.color)
+            .imageScale(.large)
+    }
+}
+
+struct TimeOfDayIcon_Previews: PreviewProvider {
+    static var previews: some View {
+        // Display a list of TimeOfDayIcon views for each hour of the day.
+        List(0..<24) { item in
+            HStack {
+                // Calculate the Date for the current hour.
+                let theDate = addHourToDate(date: Date(), numHours: item, numMinutes: 0)
+                
+                // Display the time and the TimeOfDayIcon for the current hour.
+                Text(getTimeFromDate(date: theDate))
+                Spacer()
+                TimeOfDayIcon(date: theDate)
+            }
+            .padding()
+        }
+    }
+}
